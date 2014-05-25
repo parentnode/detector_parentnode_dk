e4567822654bdc7c6e3e1a1909883793f13077bf
kaestel <kaestel@e-types.com>
2014-05-25 22:10:42 +0200

    Merged global updates

--- src/www/js/lib/seg_desktop_ie_include.js
+++ src/www/js/lib/seg_desktop_ie_include.js
@@ -5,6 +5,3 @@ document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-googleanalytics.js"></script>');
 
 document.write('<script type="text/javascript" src="http://parentnode.local/js/lib/desktop/i-page-desktop.js"></script>');
-
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-wishlist-desktop.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-todolist-desktop.js"></script>');
