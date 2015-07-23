76e36ed668cecfe429f7af09d248f37f7c0e7501
kaestel <martin@think.dk>
2015-07-23 14:43:51 +0200

    Correct api url

--- src/www/build.php
+++ src/www/build.php
@@ -117,7 +117,7 @@ if(count($action) > 0) {
 
 		$ch = curl_init();
 		curl_setopt_array($ch, array(
-			CURLOPT_URL => "http://detector-v3.api/build",
+			CURLOPT_URL => "http://detector-v3.dearapi.com/build",
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_SSL_VERIFYPEER => false,
 			CURLOPT_SSL_VERIFYHOST => 2,
