a7e16e73d292b6db0d1f6ef57e0e705bcf058476
kaestel <martin@think.dk>
2016-01-05 14:07:31 +0100

    merged

--- src/www/js/lib/seg_desktop_include.js
+++ src/www/js/lib/seg_desktop_include.js
@@ -1,8 +1,3 @@
 document.write('<script type="text/javascript" src="http://parentnode.local/js/global/seg_desktop_include.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/manipulator/src/u-events.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-movements.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-browser.js"></script>');
-
-
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-build.js"></script>');
