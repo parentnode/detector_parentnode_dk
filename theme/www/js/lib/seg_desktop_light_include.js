aea847866fec59450215ec3054ce2d47a06a3eb1
ElisabethSH <e.haabegaard@gmail.com>
2018-11-29 12:43:42 +0100

    Update js-include paths

--- theme/www/js/lib/seg_desktop_light_include.js
+++ theme/www/js/lib/seg_desktop_light_include.js
@@ -1,4 +1,4 @@
-document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/seg_desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_desktop_light.js"></script>');
 
-document.write('<script type="text/javascript" src="js/lib/desktop_light/i-front.js"></script>');
-document.write('<script type="text/javascript" src="js/lib/u-settings.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop_light/i-front.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
--- theme/www/js/lib/seg_smartphone_include.js
+++ theme/www/js/lib/seg_smartphone_include.js
@@ -1,4 +1,4 @@
-document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/seg_smartphone.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_smartphone.js"></script>');
 
-document.write('<script type="text/javascript" src="js/lib/desktop_light/i-front.js"></script>');
-document.write('<script type="text/javascript" src="js/lib/u-settings.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop_light/i-front.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
