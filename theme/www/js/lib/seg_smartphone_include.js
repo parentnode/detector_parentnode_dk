914bd2f9d1295f373c7db334efeefe401a69f01e
threepwoodd <scope.dk@gmail.com>
2019-04-01 18:42:55 +0200

    Update js includes

--- theme/www/js/lib/seg_smartphone_include.js
+++ theme/www/js/lib/seg_smartphone_include.js
@@ -4,6 +4,7 @@ document.write('<script type="text/javascript" src="/assets/parentnode-skin-defa
 
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-login.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-signup.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-verify.js"></script>');
 
 // Project
 document.write('<script type="text/javascript" src="/js/lib/smartphone/i-front.js"></script>');
--- theme/www/js/lib/seg_tablet_include.js
+++ theme/www/js/lib/seg_tablet_include.js
@@ -4,6 +4,8 @@ document.write('<script type="text/javascript" src="/assets/parentnode-skin-defa
 
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-login.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-signup.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-verify.js"></script>');
+
 
 // Project
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-front.js"></script>');
